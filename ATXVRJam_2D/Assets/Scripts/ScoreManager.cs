a8829384448838e5c4f23e8842d7aaef3e8982a1
dsteele <knight101@gmail.com>
2016-11-12 15:48:19 -0600

    Initial commit
    Initial commit, includes score manager

---
+++ ATXVRJam_2D/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager: MonoBehaviour
+{
+    public static int score;
+    Text text;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+
+        // Reset score to 0
+        score = 0;
+    }
+
+    void Update()
+    {
+        //Show the score value
+        text.text = "Score: " + score;
+    }
+}
